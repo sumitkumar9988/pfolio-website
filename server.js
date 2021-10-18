498884294a26268cd249b180ee0c75d57b67bbac
sumit <55837044+sumitkumar9988@users.noreply.github.com>
2021-10-19 00:30:46 +0530

    some other change

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express =require("express");
+const express = require("express");
 const app = express();
-const { createProxyMiddleware } =require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const server = require("http").Server(app);
 const next = require("next");
 const path = require('path')
-const dev = process.env.NODE_ENV !== "production";
+const dev =true;
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 require("dotenv").config();
@@ -24,37 +24,37 @@ const store = require('le-store-s3').create({ S3 })
 nextApp.prepare().then(() => {
 
     app.all("*", (req, res) => handle(req, res));
-    const greenlock = require('greenlock-express').create({
-        server: 'https://acme-v02.api.letsencrypt.org/directory',
-        version: 'draft-11',
-        configDir: path.join(__dirname, 'acme'),
-        approveDomains,
-        app: app.use('/',
-            createProxyMiddleware({
-                target: 'http://pfolio.site',
-                changeOrigin: true
-            })
-        ),
-        communityMember: true,
-        store,
-        debug: process.env.NODE_ENV === 'development',
-        renewBy: 10 * 24 * 60 * 60 * 1000,
-        renewWithin: 14 * 24 * 60 * 60 * 1000
-    })
+    // const greenlock = require('greenlock-express').create({
+    //     server: 'https://acme-v02.api.letsencrypt.org/directory',
+    //     version: 'draft-11',
+    //     configDir: path.join(__dirname, 'acme'),
+    //     approveDomains,
+    //     app: app.use('/',
+    //         createProxyMiddleware({
+    //             target: 'http://pfolio.site',
+    //             changeOrigin: true
+    //         })
+    //     ),
+    //     communityMember: true,
+    //     store,
+    //     debug: process.env.NODE_ENV === 'development',
+    //     renewBy: 10 * 24 * 60 * 60 * 1000,
+    //     renewWithin: 14 * 24 * 60 * 60 * 1000
+    // })
 
-    function approveDomains(opts, certs, cb) {
-        opts.challenges = { 'http-01': http01 }
-        opts.email = "sumit.firstletter@gmail.com"
-    
-        if (certs) {
-            opts.domains = [certs.subject].concat(certs.altnames)
-        }
-        opts.agreeTos = true
-        cb(null, { options: opts, certs: certs })
-    }
-    greenlock.listen(80, 443)
-    // server.listen(PORT, err => {
-    //     if (err) throw err;
-    //     console.log("Express server running");
-    // });
+    // function approveDomains(opts, certs, cb) {
+    //     opts.challenges = { 'http-01': http01 }
+    //     opts.email = "sumit.firstletter@gmail.com"
+
+    //     if (certs) {
+    //         opts.domains = [certs.subject].concat(certs.altnames)
+    //     }
+    //     opts.agreeTos = true
+    //     cb(null, { options: opts, certs: certs })
+    // }
+    // greenlock.listen(80, 443)
+    server.listen(PORT, err => {
+        if (err) throw err;
+        console.log("Express server running");
+    });
 });
