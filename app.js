5f40820ac13296be6ab44eccc3d1f9029bd79265
sumit <55837044+sumitkumar9988@users.noreply.github.com>
2021-11-13 00:20:19 +0530

    static file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express()
-// app.use(express.static('build'));
+app.use(express.static('build'));
 
 app.use(
     "/",
